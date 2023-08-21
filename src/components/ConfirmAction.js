dcdcf6a8a3b26f10df232984e68828df3f4bc154
Abdullah <abdullahfaisal7729@gmail.com>
2023-08-21 10:17:14 +0500

    fixed styling

--- src/components/ConfirmAction.js
+++ src/components/ConfirmAction.js
@@ -3,12 +3,12 @@ import React from "react";
 const ConfirmAction = ({ onClickDelete, onClickCancel }) => {
   return (
     <div class="relative px-4 min-h-full md:flex md:items-center md:justify-center">
-      <div class="bg-black opacity-25 w-full h-full absolute z-10 inset-0"></div>
-      <div class="bg-white rounded-lg md:max-w-md md:mx-auto p-4 fixed inset-x-0 bottom-0 z-50 mb-4 mx-4 md:relative">
+      <div class=" opacity-25 w-full h-full absolute z-10 inset-0"></div>
+      <div class="bg-black text-white mt-4 rounded-lg md:max-w-md md:mx-auto p-4 fixed inset-x-0 bottom-0 z-50 mb-4 mx-4 md:relative">
         <div class="md:flex items-center">
           <div class="mt-4 md:mt-0 md:ml-6 text-center md:text-left">
             <p class="font-bold">Confimation: Delete</p>
-            <p class="text-sm text-gray-700 mt-1">
+            <p class="text-sm text-gray-700 mt-1 text-white">
               You will lose all of your data by deleting. This action cannot be
               undone.
             </p>
@@ -23,7 +23,7 @@ const ConfirmAction = ({ onClickDelete, onClickCancel }) => {
           </button>
           <button
             class="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-gray-200 rounded-lg font-semibold text-sm mt-4
-          md:mt-0 md:order-1"
+          md:mt-0 md:order-1 text-black"
             onClick={onClickCancel}
           >
             Cancel
